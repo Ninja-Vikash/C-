738152c6bac1ee36f88dc38bcd552cc382bd8102
Vikash Kumar <vikashloharjsr@gmail.com>
2024-06-05 13:35:13 +0530

    escape sequence

---
+++ newline.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello Coder!\n";                // Using Escape Sequence
+    cout << "I am learing C++";
+    return 0;
+}
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     cout << "Hello Coder!" << endl;
+//     cout << "I am learing C++" << endl;
+//     cout << "This is crazy!";
+//     return 0;
+// }
